c3b15e5e8c0e8a5b9ce71a60786ce05442ce6291
Egor <egorborovoy26@gmail.com>
2016-02-19 12:46:07 +0200

    Home Work

--- BorovoyRepository/src/main/java/uits/jv1601/lessons/App.java
+++ BorovoyRepository/src/main/java/uits/jv1601/lessons/App.java
@@ -1,13 +1,15 @@
 package uits.jv1601.lessons;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello vasja!" );
+import java.util.Scanner; 
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int i ;
+        System.out.print("Введите целое число: ");
+       
+          i = sc.nextInt();
+          System.out.println(i*2);
+     
+          System.out.println("Вы ввели не целое число");
+        }
     }
-}
