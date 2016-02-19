cbd7eb53b7f8bf11315ff572299b075d2ebf375f
Egor <egorborovoy26@gmail.com>
2016-02-19 12:59:45 +0200

    Home Work

--- BorovoyRepository/src/main/java/uits/jv1601/lessons/App.java
+++ BorovoyRepository/src/main/java/uits/jv1601/lessons/App.java
@@ -5,11 +5,11 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
         int i ;
-        System.out.print("Введите целое число: ");
+        System.out.print("Enter number; ");
        
           i = sc.nextInt();
           System.out.println(i*2);
      
-          System.out.println("Вы ввели не целое число");
+       
         }
     }
