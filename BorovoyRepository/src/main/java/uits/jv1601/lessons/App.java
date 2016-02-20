585c8bbddb400badd6a9212349ac9d3b34e1cc82
Egor <egorborovoy26@gmail.com>
2016-02-20 23:01:02 +0200

    Dz

--- BorovoyRepository/src/main/java/uits/jv1601/lessons/App.java
+++ BorovoyRepository/src/main/java/uits/jv1601/lessons/App.java
@@ -1,15 +1,14 @@
 package uits.jv1601.lessons;
 
-import java.util.Scanner; 
+
+
 public class App {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); 
-        int i ;
-        System.out.print("Enter number; ");
-       
-          i = sc.nextInt();
-          System.out.println(i*22); 
-     
-       //gg wp
+  System.out.println("Hello world");
+
         }
     }
+
+
+
+
